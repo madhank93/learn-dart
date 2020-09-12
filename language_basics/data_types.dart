d2ec863650ce5e986303cf8c81db5af08d1d908e
madhank93 <maddymadhan946@gmail.com>
2020-09-12 21:31:34 +0530

    dart data types

---
+++ language_basics/data_types.dart
@@ -0,0 +1,23 @@
+/*
+    Number    -	int, double, num	Numbers in Dart are used to represent numeric literals
+    Strings	  - String	Strings represent a sequence of characters
+    Booleans	- bool	It represents Boolean values true and false
+    Lists	    - List	It is an ordered group of objects
+    Maps	    - Map	It represents a set of values as key-value pairs
+*/
+void main() { 
+    
+   // declare an integer 
+   int num1 = 2;              
+       
+   // declare a double value 
+   double num2 = 1.5;
+
+   // 
+   num val = 2.3;
+  
+   // print the values 
+   print(num1); 
+   print(num2);
+   print(val);
+} 
