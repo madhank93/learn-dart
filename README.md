bc5d770be8d3eea2f87fae23981be1528e7b0bf0
madhank93 <maddymadhan946@gmail.com>
2020-09-27 20:02:30 +0530

    adding readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dart
+
+## Table of Contents
+
+### Language basics
+
+        - Data types
+        - Variable types
--- language_basics/data_types.dart
+++ language_basics/data_types.dart
@@ -1,9 +1,16 @@
 /*
+    Dart core lib -https://api.dart.dev/stable/2.9.3/dart-core/dart-core-library.html
+    Dart tour - https://dart.dev/guides/language/language-tour
+    
+    Built-in types:
+
     Number    -	int, double, num in Dart are used to represent numeric literals
     Strings	  - String represent a sequence of characters
     Booleans	- bool represents Boolean values true and false
     Lists	    - List is an ordered group of objects
     Maps	    - Map represents a set of values as key-value pairs
+    Set       - A collection of objects in which each object can occur only once.
+
 */
 void main() {
   // an integer variable, whole value
@@ -27,12 +34,15 @@ void main() {
   // Collection
 
   // List is a index based group of objects. for declaring list need to use [ ]
+  // https://api.dart.dev/stable/2.9.3/dart-core/List-class.html
   List cities = ["Chennai", "Delhi", "Goa"];
 
   // Map stores the values in a key-value pair. for declaring map need to use { }
+  // https://api.dart.dev/stable/2.9.3/dart-core/Map-class.html
   Map cityAndPincode = {"Chennai": 600005, "Delhi": 700056, "Bhopal": 988905};
 
   // Set represents a collection of objects in which each object can occur only once.
+  // https://api.dart.dev/stable/2.9.3/dart-core/Set-class.html
   Set schoolID = {523,164,614};
   Set registerNumber = Set.from([585,9884,9899,1452]);
 
